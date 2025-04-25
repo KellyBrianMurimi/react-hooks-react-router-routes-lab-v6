b02ff8371c799664dd5cccd27c2188db266057b9
KellyBrianMurimi <kellybrianmurimi@gmail.com>
2025-04-26 00:45:48 +0300

    Just

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,14 @@
+ import Home from "./pages/Home";
+ import Movie from "./pages/Movie";
+ import Actors from "./pages/Actors";
+ import Directors from "./pages/Directors";
 
 
 const routes = [
-   
-  ];
+  { path: "/", element: <Home /> },
+  { path: "/actors", element: <Actors /> },
+  { path: "/directors", element: <Directors /> },
+  { path: "/movie/:id", element: <Movie /> }
+];
 
 export default routes;
